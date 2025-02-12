e9d31bff85ed8be709cb825b1c01816abd800c61
Mayravh71 <106397591+Mayravh71@users.noreply.github.com>
2025-02-12 11:27:18 -0600

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://mayravh71.github.io/c-11bloques-de-obstculos/](https://mayravh71.github.io/c-11bloques-de-obstculos/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://minecraft.makecode.com/](https://minecraft.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/mayravh71/c-11bloques-de-obstculos** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://minecraft.makecode.com/](https://minecraft.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/mayravh71/c-11bloques-de-obstculos** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/minecraft
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,88 @@
+function construir_obstaculo () {
+    for (let value of Blocks) {
+        blocks.place(value, loc)
+        blocks.place(BEACON, positions.add(
+        loc,
+        pos(0, 0, -2)
+        ))
+        blocks.place(BEACON, positions.add(
+        loc,
+        pos(0, 0, 2)
+        ))
+        block_loc.push(loc)
+        loc = positions.add(
+        loc,
+        pos(5, 0, 0)
+        )
+    }
+    blocks.place(ENCHANTMENT_TABLE, positions.add(
+    loc,
+    pos(1, 0, 0)
+    ))
+    loops.pause(100)
+    count = 0
+    time = 30
+}
+player.onChat("obstaculo", function () {
+    start_position = player.position()
+    loc = positions.add(
+    player.position(),
+    pos(1, 0, 0)
+    )
+    construir_obstaculo()
+})
+loops.forever(function () {
+    for (let index = 0; index < 31; index++) {
+        loops.pause(1000)
+        if (time > 0) {
+            time += -1
+            player.say("Tiempo restante " + time + " segundos")
+            if (time == 0) {
+                player.teleport(start_position)
+                time = -1
+            }
+        }
+    }
+})
+function remove_blocks () {
+    blocks.place(AIR, positions.add(
+    loc,
+    pos(1, 0, 0)
+    ))
+    for (let value of block_loc) {
+        blocks.place(AIR, value)
+    }
+}
+let start_position: Position = null
+let time = 0
+let count = 0
+let loc: Position = null
+let block_loc: Position[] = []
+let Blocks: number[] = []
+gameplay.setGameMode(
+CREATIVE,
+mobs.target(ALL_PLAYERS)
+)
+mobs.teleportToPlayer(
+mobs.target(ALL_PLAYERS),
+mobs.target(LOCAL_PLAYER)
+)
+Blocks = [
+PLANKS_OAK,
+PLANKS_DARK_OAK,
+POLISHED_ANDESITE,
+NOTE_BLOCK,
+GRANITE,
+LAPIS_LAZULI_BLOCK,
+PLANKS_SPRUCE
+]
+block_loc = []
+player.execute(
+"/scoreboard objectives add count dummy count"
+)
+player.execute(
+"/scoreboard players set @a count 0"
+)
+player.execute(
+"/scoreboard objectives setdisplay sidebar count "
+)
